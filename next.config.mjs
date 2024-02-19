90954abf5b5b1c9ee0d7755f6e3eeb2ef13db23c
Raghav Jindal <raghavjindal0212@gmail.com>
2024-02-19 18:44:36 +0530

    Frames testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/cosmiccowboys',
+                destination: 'https://github.com/HiberNuts',
+                permanent: false
+            },
+            {
+                source: '/pinatacloud',
+                destination: 'https://github.com/HiberNuts',
+                permanent: false
+            },
+            {
+                source: '/video',
+                destination: 'https://github.com/HiberNuts',
+                permanent: false
+            }
+        ]
+    }
+};
 
 export default nextConfig;
